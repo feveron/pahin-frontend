41b22130b8b728b7c3b8c60b2b10e86f86c897b5
feveron <veronika.fedyk06@gmail.com>
2026-04-05 01:59:44 +0300

    chore: setup tailwind css

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
+    <div className="justify-center items-center flex flex-col gap-4 py-6">
       <nav>
         <Link to="/">Home</Link> |{" "}
         <Link to="/map">Map</Link> |{" "}
--- src/pages/profile/ProfileLayout.tsx
+++ src/pages/profile/ProfileLayout.tsx
@@ -2,12 +2,12 @@ import { Link, Outlet } from "react-router-dom"
 
 export function ProfileLayout() {
   return (
-    <div>
+    <div className="text-green-700 justify-center items-center flex flex-col gap-4">
       <h2>Profile</h2>
 
       <nav>
-        <Link to=".">Info</Link> |{" "}
-        <Link to="settings">Settings</Link>
+        <Link to=".">Trees</Link> |{" "}
+        <Link to="settings">Settings</Link> |{" "}
         <Link to="certificates">Certificates</Link>
       </nav>
 
