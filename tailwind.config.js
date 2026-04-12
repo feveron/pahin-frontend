73dc364715a30eb096d3957e9a9199547819dc17
Olha Harnyk <garnik12122005@gmail.com>
2026-04-12 23:46:33 +0300

    feat: configure basic project styles with custom fonts and color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,26 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          DEFAULT: "#105339",
+          hover: "#2D6A4F",
+          light: "#B0F1CC",
+        },
+        cream: {
+          DEFAULT: "#FEFAE8",
+          input: "#F2EEDD", //inputs,
+          comment: "#ECE8D8", //коментар у формі, кнопка поділитись сертифікатом в соц.м.
+          footer: "#F8F4E3", //footer
+        },
+        brown: "#683C2A",
+      },
+      fonts: {
+        heading: ["Manrope", "sans-serif"],
+        text: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
