d75c44e2893684776eef79ae9c41e6eb2e077a45
feveron <veronika.fedyk06@gmail.com>
2026-05-11 04:03:08 +0300

    fix: update import statement for MiniCard in Step1SelectTree component

--- src/components/StepsForm/Step1SelectTree.tsx
+++ src/components/StepsForm/Step1SelectTree.tsx
@@ -2,7 +2,7 @@ import type { FormikProps } from "formik"
 import type { PlantTreeValues, Species } from "../../types/tree"
 import { useSpecies } from "../../hooks/useSpecies" // хук для отримання видів з беку
 import { useEffect, useRef } from "react"
-import { MiniCard } from "../TreeCards/MiniCard"
+import { MiniCard } from "../TreeCards/"
 
 interface Props {
   formik: FormikProps<PlantTreeValues>
